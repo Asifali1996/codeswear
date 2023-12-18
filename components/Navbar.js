984cba2834c7aed37135e7a44e85f3fbcddf06af
Asifali1996 <asifalikhan090@gmail.com>
2023-12-18 12:30:01 +0500

    navbar and footer added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsCart4 } from "react-icons/bs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md">
+      <div className="logo mx-5">
+        <Link href={"/"}>
+          <a>
+            <Image src="/logo.png" alt="" width={200} height={40} />
+          </a>
+        </Link>
+      </div>
+      <div className="nav">
+        <ul className="flex items-center space-x-6 font-bold md:text-md">
+          <Link href={"/tshirts"}>
+            <a>
+              <li>Tshirts</li>
+            </a>
+          </Link>
+          <Link href={"/hoodies"}>
+            <a>
+              <li>Hoodies</li>
+            </a>
+          </Link>
+          <Link href={"/mugs"}>
+            <a>
+              <li>Mugs</li>
+            </a>
+          </Link>
+          <Link href={"/stickers"}>
+            <a>
+              <li>Stickers</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+      <div className="cart absolute mx-5 right-0 top-2">
+        <BsCart4 className="text-3xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
